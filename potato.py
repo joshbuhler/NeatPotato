bad6708650d45d929c8d0fc7f2f8e1e21d9fc4cf
Joshua Buhler <josh@buhler.me>
2022-11-20 08:12:51 -0700

    parsity parse

--- potato.py
+++ potato.py
@@ -16,10 +16,31 @@ parentGNode = domData.documentElement.getElementsByTagName("g")[0]
 
 weekGNodes = parentGNode.getElementsByTagName("g")
 
+
+currentX = 0
+currentY = 0
+
+# All units are in meters
+borderSize = 0
+spacing = 1
+towerSize = 2
+
+currentX += borderSize
+currentY += borderSize
+
 for week in weekGNodes:
 	dayRects = week.getElementsByTagName("rect")
 	for day in dayRects:
 		date = day.getAttribute("data-date")
 		dataLevel = day.getAttribute("data-level")
 
-		print("Date: " + date + " Level: " + dataLevel)
+		print("Date: " + date + " Level: " + dataLevel + " x|y: %2d|%2d" % (currentX, currentY))
+
+		currentY += towerSize
+		currentY += spacing
+
+	# week ended - reset
+	currentY = borderSize
+
+	currentX += towerSize
+	currentX += spacing
