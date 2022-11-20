c3340134072fe4e1eb857744c06185bc77574f1b
Joshua Buhler <josh@buhler.me>
2022-11-20 08:21:54 -0700

    Runs in blender, adds cube meshes

--- potato.py
+++ potato.py
@@ -1,4 +1,4 @@
-##import bpy
+import bpy
 import os
 import sys
 from xml.dom.minidom import parse
@@ -9,7 +9,8 @@ from os.path import isfile, join
 
 print ("What is it with you and potatoes?")
 
-filePath = "testSvg/graph1.svg"
+filePath = "/Users/josh/Projects/stunning-potato/testSvg/graph1.svg"
+#filePath = "testSvg/graph1.svg"
 domData = parse(filePath)
 
 parentGNode = domData.documentElement.getElementsByTagName("g")[0]
@@ -17,30 +18,37 @@ parentGNode = domData.documentElement.getElementsByTagName("g")[0]
 weekGNodes = parentGNode.getElementsByTagName("g")
 
 
-currentX = 0
-currentY = 0
+currentX = 0.0
+currentY = 0.0
 
 # All units are in meters
-borderSize = 0
-spacing = 1
-towerSize = 2
+borderSize = 0.0
+spacing = 1.0
+towerSize = 2.0
 
 currentX += borderSize
 currentY += borderSize
 
 for week in weekGNodes:
-	dayRects = week.getElementsByTagName("rect")
-	for day in dayRects:
-		date = day.getAttribute("data-date")
-		dataLevel = day.getAttribute("data-level")
+    dayRects = week.getElementsByTagName("rect")
+    for day in dayRects:
+        date = day.getAttribute("data-date")
+        dataLevel = float(day.getAttribute("data-level"))
 
-		print("Date: " + date + " Level: " + dataLevel + " x|y: %2d|%2d" % (currentX, currentY))
+        print("Date: " + date + " Level: %2d" % (dataLevel) + " x|y: %2d|%2d" % (currentX, currentY))
+        
+        bpy.ops.mesh.primitive_cube_add(size=towerSize,
+            enter_editmode=False,
+            align='WORLD',
+            location=(currentX, currentY, 0),
+            scale=(1, 1, dataLevel))
 
-		currentY += towerSize
-		currentY += spacing
+        currentY += towerSize
+        currentY += spacing
 
-	# week ended - reset
-	currentY = borderSize
+    # week ended - reset
+    currentY = borderSize
 
-	currentX += towerSize
-	currentX += spacing
+    currentX += towerSize
+    currentX += spacing
+    
