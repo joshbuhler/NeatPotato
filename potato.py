7a31dd20db7a103e47f2b9009611686bfefc0a8e
Joshua Buhler <josh@buhler.me>
2022-12-03 08:09:12 -0700

    Running full script again

--- potato.py
+++ potato.py
@@ -8,83 +8,79 @@ from os.path import isfile, join
 
 
 
-
-filePath = "/Users/josh/Projects/stunning-potato/testSvg/graph1.svg"
-
 class NeatPotato:
+
+    __slots__ = ('filePath')
 
     def __init__(self, blenderContext, svgFilepath):
         print ("What is it with you and potatoes?")
 
         print("🥔 Loading SVG: " + svgFilepath)
 
+        self.filePath = svgFilepath
 
+    def parse(self):
+        print("🍟 Parsing: " + self.filePath)
+        domData = parse(self.filePath)
 
+        parentGNode = domData.documentElement.getElementsByTagName("g")[0]
+        weekGNodes = parentGNode.getElementsByTagName("g")
 
+        currentX = 0.0
+        currentY = 0.0
 
-# #filePath = "testSvg/graph1.svg"
-# domData = parse(filePath)
+        # All units are in meters
+        borderSize = 1.0
+        spacing = 1.0
+        towerSize = 2.0
+        minHeight = 2.0
 
-# parentGNode = domData.documentElement.getElementsByTagName("g")[0]
+        currentX += borderSize
+        currentY += borderSize
 
-# weekGNodes = parentGNode.getElementsByTagName("g")
+        maxCurrentY = 0.0
 
+        applyBevel = False
 
-# currentX = 0.0
-# currentY = 0.0
+        for week in weekGNodes:
+            dayRects = week.getElementsByTagName("rect")
+            
+            # reset for a new week
+            currentY = borderSize
+            
+            for day in dayRects:
+                date = day.getAttribute("data-date")
+                dataLevel = float(day.getAttribute("data-level")) + minHeight
 
-# # All units are in meters
-# borderSize = 1.0
-# spacing = 1.0
-# towerSize = 2.0
-# minHeight = 2.0
-
-# currentX += borderSize
-# currentY += borderSize
-
-# maxCurrentY = 0.0
-
-# applyBevel = False
-
-# for week in weekGNodes:
-#     dayRects = week.getElementsByTagName("rect")
-    
-#     # reset for a new week
-#     currentY = borderSize
-    
-#     for day in dayRects:
-#         date = day.getAttribute("data-date")
-#         dataLevel = float(day.getAttribute("data-level")) + minHeight
-
-#         print("Date: " + date + " Level: %2d" % (dataLevel) + " x|y: %2d|%2d" % (currentX, currentY))
-        
-#         bpy.ops.mesh.primitive_cube_add(size=towerSize,
-#             enter_editmode=False,
-#             align='WORLD',
-#             location=(currentX + (towerSize / 2), currentY + (towerSize / 2), 0),
-#             scale=(1, 1, dataLevel))
+                print("Date: " + date + " Level: %2d" % (dataLevel) + " x|y: %2d|%2d" % (currentX, currentY))
                 
-#         if applyBevel:
-#             bpy.ops.object.modifier_add(type="BEVEL")
-#             bpy.context.object.modifiers["Bevel"].segments = 10
+                bpy.ops.mesh.primitive_cube_add(size=towerSize,
+                    enter_editmode=False,
+                    align='WORLD',
+                    location=(currentX + (towerSize / 2), currentY + (towerSize / 2), 0),
+                    scale=(1, 1, dataLevel))
+                        
+                if applyBevel:
+                    bpy.ops.object.modifier_add(type="BEVEL")
+                    bpy.context.object.modifiers["Bevel"].segments = 10
 
-#         currentY += towerSize
-#         currentY += spacing
-        
-#         if currentY >= maxCurrentY:
-#             maxCurrentY = currentY
+                currentY += towerSize
+                currentY += spacing
+                
+                if currentY >= maxCurrentY:
+                    maxCurrentY = currentY
 
-#     currentX += towerSize
-#     currentX += spacing
-    
-# # Put a base on this thing
-# bpy.ops.mesh.primitive_cube_add(size=1.0,
-#     enter_editmode=False,
-#     align='WORLD',
-#     location=(currentX / 2, maxCurrentY / 2, 0),
-#     scale=(currentX, maxCurrentY, minHeight))
-    
-    
-    
+            currentX += towerSize
+            currentX += spacing
+            
+        # Put a base on this thing
+        bpy.ops.mesh.primitive_cube_add(size=1.0,
+            enter_editmode=False,
+            align='WORLD',
+            location=(currentX / 2, maxCurrentY / 2, 0),
+            scale=(currentX, maxCurrentY, minHeight))
+            
+            
+            
 
-# print ("I just think they're neat!")
+        print ("🥔 I just think they're neat!")
